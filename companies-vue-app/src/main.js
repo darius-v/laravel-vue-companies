69c68f9f0232515ae1ab5288898d1c763e4baa55
Darius Valancauskas <darius.veracity@gmail.com>
2021-09-07 14:29:17 +0300

    Bootstrap theme

--- companies-vue-app/src/main.js
+++ companies-vue-app/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/themes/bootstrap4-light-blue/theme.css'
 import 'primevue/resources/primevue.min.css'                 // core css
 import 'primeicons/primeicons.css'
 
