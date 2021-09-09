da1667ed1b8cbf952ae2ee0a7c136b1fc580d653
Darius Valancauskas <darius.veracity@gmail.com>
2021-09-09 11:02:47 +0300

    In progress creating repository

---
+++ app/Repository/Eloquent/BaseRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repository\Eloquent;
+
+use App\Repository\EloquentRepositoryInterface;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseRepository implements EloquentRepositoryInterface
+{
+    public function __construct(protected Model $model)
+    {
+    }
+}
